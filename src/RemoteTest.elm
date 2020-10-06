2146d0fec721d87e1fe22580ee58d0d93496be16
Adam Christopher <adam.keristoper@gmail.com>
2020-10-06 15:02:55 +0900

    Add remote data

---
+++ src/RemoteTest.elm
@@ -0,0 +1,173 @@
+module RemoteTest exposing (main)
+
+import Browser
+import Html exposing (..)
+import Html.Events exposing (onClick)
+import Http
+import Json.Decode as Decode
+    exposing
+        ( Decoder
+        , decodeString
+        , field
+        , int
+        , list
+        , map3
+        , string
+        )
+import RemoteData exposing (..)
+
+
+type alias Post =
+    { id : Int
+    , title : String
+    , author : String
+    }
+
+
+type alias Model =
+    { posts : WebData (List Post)
+    , errorMessage : Maybe String
+    }
+
+
+
+-- UPDATE
+
+
+type Msg
+    = SendHttpRequest
+    | RemoteDataReceived (WebData (List Post))
+
+
+postDecoder : Decoder Post
+postDecoder =
+    Decode.map3 Post
+        (field "id" int)
+        (field "title" string)
+        (field "author" string)
+
+
+httpCommand : Cmd Msg
+httpCommand =
+    Http.get
+        { url = "http://localhost:3003/posts.json"
+        , expect = Http.expectJson (RemoteData.fromResult >> RemoteDataReceived) (list postDecoder)
+        }
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        SendHttpRequest ->
+            ( model, httpCommand )
+
+        RemoteDataReceived response ->
+            ( { model | posts = response }
+            , Cmd.none
+            )
+
+
+buildErrorMessage : Http.Error -> String
+buildErrorMessage httpError =
+    case httpError of
+        Http.BadUrl message ->
+            message
+
+        Http.Timeout ->
+            "Server is taking too long to respond. Please try again later."
+
+        Http.NetworkError ->
+            "Unable to reach server."
+
+        Http.BadStatus statusCode ->
+            "Request failed with status code: " ++ String.fromInt statusCode
+
+        Http.BadBody message ->
+            message
+
+
+
+-- VIEW
+
+
+viewPost : Post -> Html Msg
+viewPost post =
+    tr []
+        [ td [] [ text (String.fromInt post.id) ]
+        , td [] [ text post.title ]
+        , td [] [ text post.author ]
+        ]
+
+
+viewTableHeader : Html Msg
+viewTableHeader =
+    tr []
+        [ th [] [ text "ID" ]
+        , th [] [ text "Title" ]
+        , th [] [ text "Author" ]
+        ]
+
+
+viewPosts : List Post -> Html Msg
+viewPosts posts =
+    div []
+        [ h3 [] [ text "Posts" ]
+        , table []
+            ([ viewTableHeader ] ++ List.map viewPost posts)
+        ]
+
+
+viewError : String -> Html Msg
+viewError message =
+    let
+        errorHeading =
+            "Couldn't fetch data at this time"
+    in
+    div []
+        [ h3 [] [ text errorHeading ]
+        , text ("Error: " ++ message)
+        ]
+
+
+viewPostsOrError : Model -> Html Msg
+viewPostsOrError model =
+    case model.posts of
+        NotAsked ->
+            text "Click to load posts"
+
+        Loading ->
+            text "Loading"
+
+        Failure httpError ->
+            text ("Error: " ++ buildErrorMessage httpError)
+
+        Success posts ->
+            viewPosts posts
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ button [ onClick SendHttpRequest ]
+            [ text "Get data from server" ]
+        , viewPostsOrError model
+        ]
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( { posts = NotAsked
+      , errorMessage = Nothing
+      }
+    , Cmd.none
+    )
+
+
+main : Program () Model Msg
+main =
+    Browser.element
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = \_ -> Sub.none
+        }
