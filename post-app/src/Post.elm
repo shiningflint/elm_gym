49a777f6c154c6d20aec4d59fa1f07d1a3236b6d
Adam Christopher <adam.keristoper@gmail.com>
2020-10-07 14:41:17 +0900

    Move Files to create a new lem project

---
+++ post-app/src/Post.elm
@@ -0,0 +1,24 @@
+module Post exposing (Post, postDecoder, postsDecoder)
+
+import Json.Decode as Decode exposing (Decoder, int, list, string)
+import Json.Decode.Pipeline exposing (required)
+
+
+type alias Post =
+    { id : Int
+    , title : String
+    , author : String
+    }
+
+
+postsDecoder : Decoder (List Post)
+postsDecoder =
+    list postDecoder
+
+
+postDecoder : Decoder Post
+postDecoder =
+    Decode.succeed Post
+        |> required "id" int
+        |> required "title" string
+        |> required "author" string
