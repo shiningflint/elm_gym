5b292a1ff7ed655068b58a56de4a652c5fe27af8
Adam Christopher <adam.keristoper@gmail.com>
2020-11-25 12:17:18 +0900

    Move opaque learning files to its own folder

--- opaque/src/Main.elm
+++ opaque/src/Main.elm
@@ -3,11 +3,19 @@
 -- refer to
 -- Opaque type for validation
 -- https://dev.to/izumisy/designing-opaque-type-for-form-fields-in-elm-4299
+-- We learned about extracting a functional component of "Username".
+-- Setting opaque type of "Username" with possabilities being Idle, Valid, or Invalid.
+--
+-- In Part 2, We are going to add email field under the same form.
+-- But instead of copying over everything from Username field,
+-- We're going to use "module composition".
+-- On which Username and Email will share some similar attributes
+--
 -- Further reading:
 -- https://medium.com/@ckoster22/advanced-types-in-elm-opaque-types-ec5ec3b84ed2
 
 
-module UsernameValidationOpaque exposing (main)
+module Main exposing (main)
 
 import Browser
 import Html exposing (..)
